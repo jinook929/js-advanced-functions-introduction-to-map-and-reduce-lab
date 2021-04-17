23c4e71ba99f57727614017063722f363559da95
jinook929 <jinook929@gmail.com>
2021-04-17 00:24:49 -0400

    commit tree 0656cd8e90b4dbecadb2351e7b5ac78d1fed6f41

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
 // Your code here
+function mapToNegativize(sourceArray) {
+  const result = []
+  sourceArray.forEach(e => result.push(-e))
+  return result
+}
+
+function mapToNoChange(sourceArray) {
+  const result = []
+  sourceArray.forEach(e => result.push(e))
+  return result
+}
+
+function mapToDouble(sourceArray) {
+  const result = []
+  sourceArray.forEach(e => result.push(e * 2))
+  return result
+}
+
+function mapToSquare(sourceArray) {
+  const result = []
+  sourceArray.forEach(e => result.push(e * e))
+  return result
+}
+
+function reduceToTotal(sourceArray, startingPoint = 0) {
+  // if(!startingPoint) {
+  //   startingPoint = 0
+  // }
+  sourceArray.forEach(e => startingPoint += e)
+  return startingPoint
+}
+
+function reduceToAllTrue(sourceArray) {
+  for(const e of sourceArray) {
+    if(!e) return false
+  }
+  return true
+}
+// reduceToAllTrue([1, 2, true, false, "razmatazz"])
+function reduceToAnyTrue(sourceArray) {
+  for(const e of sourceArray) {
+    if(!!e) return true
+  }
+  // for(let i = 0; i < sourceArray.length;i++) {
+  //   if(!!sourceArray[i]) return true
+  // }
+  return false
+}
